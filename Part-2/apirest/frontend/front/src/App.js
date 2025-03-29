927b691afb42a123d45ec2eecf92520339a5c9cf
LaraGomezN <111064261+LaraGomezN@users.noreply.github.com>
2025-03-29 14:27:45 -0500

    Front first endpoint

---
+++ Part-2/apirest/frontend/front/src/App.js
@@ -0,0 +1,11 @@
+import Predictor from "./components/Predictor";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-500 p-4">
+      <Predictor />
+    </div>
+  );
+}
+
+export default App;
